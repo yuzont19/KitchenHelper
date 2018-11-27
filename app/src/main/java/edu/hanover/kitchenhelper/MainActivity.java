0e0388a6bb6a16c4115c10bbbee9aae29b77ecca
yuzont19 <yuzont19@hanover.edu>
2018-11-27 18:42:35 -0500

    Ref #2 worked on the MainActivity.java to add functions for each separate button on the main screen

--- app/src/main/java/edu/hanover/kitchenhelper/MainActivity.java
+++ app/src/main/java/edu/hanover/kitchenhelper/MainActivity.java
@@ -2,6 +2,7 @@ package edu.hanover.kitchenhelper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Called when the search recipe button is clicked
+    public void onRecipeClick(View view) {
+        // Do something in response to button click
+    }
+
+    //Called when the favorite recipe button is clicked
+    public void onFavoriteClick(View view) {
+        // Do something in response to button click
+    }
+
+    //Called when the shopping list button is clicked
+    public void onShopClick(View view) {
+        // Do something in response to button click
+    }
+
+    //Called when the measurement converter button is clicked
+    public void onMeasureClick(View view) {
+        // Do something in response to button click
     }
 }
