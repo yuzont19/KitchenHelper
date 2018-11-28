8f974fe312ed020867fa9efe7ce07535570d3d59
yuzont19 <yuzont19@hanover.edu>
2018-11-28 11:00:10 -0500

    Ref #6 Added the Measurement Converter screen files

---
+++ app/src/main/java/edu/hanover/kitchenhelper/MeasurementConverter.java
@@ -0,0 +1,13 @@
+package edu.hanover.kitchenhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeasurementConverter extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurement_converter);
+    }
+}
