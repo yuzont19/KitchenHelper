c1fab0177630bc54fd3e223bf18d633d6eae0279
yuzont19 <yuzont19@hanover.edu>
2018-11-28 10:57:15 -0500

    Ref #4 Added the Favorite Recipes screen

---
+++ app/src/main/java/edu/hanover/kitchenhelper/FavoriteRecipes.java
@@ -0,0 +1,13 @@
+package edu.hanover.kitchenhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FavoriteRecipes extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_recipes);
+    }
+}
