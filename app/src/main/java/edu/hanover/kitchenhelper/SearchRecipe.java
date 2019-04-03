a867fa69ab31a944cf4fa04ba0a5e23641087b19
Trixia Yuzon <yuzont19@hanover.edu>
2019-04-03 10:23:26 -0400

    Ref #14 Fixed code to get ingredients from EditText to get query

--- app/src/main/java/edu/hanover/kitchenhelper/SearchRecipe.java
+++ app/src/main/java/edu/hanover/kitchenhelper/SearchRecipe.java
@@ -3,6 +3,7 @@ package edu.hanover.kitchenhelper;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.util.ArrayList;
 import static edu.hanover.kitchenhelper.R.*;
@@ -17,10 +18,12 @@ public class SearchRecipe extends Activity {
 
     public void onSearchClick(View view) {
         ArrayList<String> ingredients = new ArrayList<>();
-//        String firstIng = findViewById(id.first_ing).toString();
-//        String secondIng = findViewById(id.second_ing).toString();
-//        ingredients.add(firstIng);
-//        ingredients.add(secondIng);
+        EditText first = (EditText) findViewById(id.first_ing);
+        String firstIng = first.getText().toString();
+        EditText second = (EditText) findViewById(id.second_ing);
+        String secondIng = second.getText().toString();
+        ingredients.add(firstIng);
+        ingredients.add(secondIng);
         Query two_ing = new Query(ingredients);
         String queryResult = two_ing.getQueryString();
         TextView result = findViewById(id.result);
