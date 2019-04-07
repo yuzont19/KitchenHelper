031f639739044da9a4286ed419c54bbf0c019b2d
Trixia Yuzon <yuzont19@hanover.edu>
2019-04-07 14:23:27 -0400

    Ref #17 added the onClickAddIngredient function for the class

--- app/src/main/java/edu/hanover/kitchenhelper/ShoppingList.java
+++ app/src/main/java/edu/hanover/kitchenhelper/ShoppingList.java
@@ -2,6 +2,8 @@ package edu.hanover.kitchenhelper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ShoppingList extends Activity {
 
@@ -9,5 +11,15 @@ public class ShoppingList extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+    }
+
+    public void onClickAddIngredient(View view) {
+        IngredientsDatabase dbHandler = new IngredientsDatabase(this);
+        EditText name = (EditText) findViewById(R.id.ingredient);
+        String ingredientName = name.getText().toString();
+        EditText quantity = (EditText) findViewById(R.id.quantity);
+        Integer quantityAmount = Integer.parseInt(quantity.getText().toString());
+        Ingredients ingredient = new Ingredients(ingredientName, quantityAmount, false, 1);
+        dbHandler.addIngredients(ingredient);
     }
 }
