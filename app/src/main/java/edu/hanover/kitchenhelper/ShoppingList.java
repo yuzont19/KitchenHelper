a6059210b528b0ef4580f3da0733ceca2f9da657
Trixia Yuzon <yuzont19@hanover.edu>
2019-04-07 20:50:52 -0400

    Ref #17 Added onClickLoadDatabase function; currently does not do what it's supposed to do

--- app/src/main/java/edu/hanover/kitchenhelper/ShoppingList.java
+++ app/src/main/java/edu/hanover/kitchenhelper/ShoppingList.java
@@ -1,7 +1,9 @@
 package edu.hanover.kitchenhelper;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,12 +16,17 @@ public class ShoppingList extends Activity {
     }
 
     public void onClickAddIngredient(View view) {
-        IngredientsDatabase dbHandler = new IngredientsDatabase(this);
+        DatabaseHelper dbHandler = new DatabaseHelper(this);
         EditText name = (EditText) findViewById(R.id.ingredient);
         String ingredientName = name.getText().toString();
         EditText quantity = (EditText) findViewById(R.id.quantity);
         Integer quantityAmount = Integer.parseInt(quantity.getText().toString());
         Ingredients ingredient = new Ingredients(ingredientName, quantityAmount, false, 1);
         dbHandler.addIngredients(ingredient);
+    }
+
+    public void onClickLoadDatabase(View view) {
+        DatabaseHelper dbHandler = new DatabaseHelper(this);
+        dbHandler.loadHandler();
     }
 }
